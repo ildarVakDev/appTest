79b0947250ec1b2ecdb5f4b5b0b48d7afaf3d2a6
Andry <gaxeyat298@chordmi.com>
2020-05-31 21:29:30 +0300

    Updates

---
+++ precache-manifest.591850a83cdefd30eeb3f4036f06ff01.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbdedccc975780a6b534995e8888de3d",
+    "url": "/IldarVakarinDev418565/index.html"
+  },
+  {
+    "revision": "1e0f7ae9fbab6437cc98",
+    "url": "/IldarVakarinDev418565/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "18dac05ed5166538babd",
+    "url": "/IldarVakarinDev418565/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "1e0f7ae9fbab6437cc98",
+    "url": "/IldarVakarinDev418565/static/js/2.59e418d5.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/IldarVakarinDev418565/static/js/2.59e418d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18dac05ed5166538babd",
+    "url": "/IldarVakarinDev418565/static/js/main.942907f3.chunk.js"
+  },
+  {
+    "revision": "f7834079e1d0f1893cf3",
+    "url": "/IldarVakarinDev418565/static/js/runtime-main.a96734aa.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/IldarVakarinDev418565/static/media/persik.4e1ec840.png"
+  }
+]);
